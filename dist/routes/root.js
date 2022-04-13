fef1cf462b5282e9de71a2bdf2797b21f6399bab
rmaejima <ryo.maejii@gmail.com>
2022-04-13 22:51:22 +0900

    Delete dist

--- dist/routes/root.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const root = async (fastify) => {
-    fastify.get("/", async (_, reply) => {
-        reply.send("Hello world!");
-    });
-};
-exports.default = root;
-//# sourceMappingURL=root.js.map
