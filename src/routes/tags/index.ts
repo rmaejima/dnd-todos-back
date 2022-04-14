26e6cf9a3f5358d2701196548bde38f7a92445c4
rmaejima <ryo.maejii@gmail.com>
2022-04-14 16:39:26 +0900

    Add get all tags API

---
+++ src/routes/tags/index.ts
@@ -0,0 +1,32 @@
+import { FastifyPluginAsync } from 'fastify';
+import { prisma } from '../../utils/prisma';
+import {
+  TodoCreateRequest,
+  TodoCreateRequestSchema,
+  TodoPayload,
+  TodoSchema,
+} from '../../types/todo';
+import { serializeDateProps } from '../../utils/serializeDate';
+import { Type } from '@sinclair/typebox';
+import { Tag } from '@prisma/client';
+import { TagSchema } from '../../types/tag';
+
+const root: FastifyPluginAsync = async (fastify) => {
+  // Get all tags API
+  fastify.get<{
+    Reply: Tag[];
+  }>(
+    '/',
+    {
+      schema: {
+        response: { 200: Type.Array(TagSchema) },
+      },
+    },
+    async (_, reply) => {
+      const allTags = await prisma.tag.findMany();
+      reply.send(allTags);
+    },
+  );
+};
+
+export default root;
