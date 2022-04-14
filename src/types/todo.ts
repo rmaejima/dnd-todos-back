856a2dd14b6bf751f24d52554b57978d0ea3c6f9
rmaejima <ryo.maejii@gmail.com>
2022-04-14 13:15:11 +0900

    Add archived

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,11 +1,12 @@
 import { Static, Type } from '@sinclair/typebox';
-import { nullable } from '../utils/typbox';
+import { nullable } from '../utils/typebox';
 import { TagSchema } from './tag';
 
 export const TodoSchema = Type.Object({
   id: Type.Number(),
   title: Type.String(),
   finished: Type.Boolean(),
+  archived: Type.Boolean(),
   createdAt: Type.String({ format: 'date-time' }),
   updatedAt: Type.String({ format: 'date-time' }),
   tags: Type.Array(TagSchema),
