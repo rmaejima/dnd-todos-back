7ee7be84b44072ef3214aa8928f32a2c0c416ae2
rmaejima <ryo.maejii@gmail.com>
2022-04-16 11:01:42 +0900

    Make sure to use common schema

--- src/routes/todos/finish/index.ts
+++ src/routes/todos/finish/index.ts
@@ -1,30 +1,40 @@
 import { FastifyPluginAsync } from 'fastify';
-import { TodoPayload, TodoSchema } from '../../../types/todo';
+import {
+  TodoFinishRequest,
+  TodoFinishRequestSchema,
+  TodoPayload,
+  TodoSchema,
+} from '../../../types/todo';
 import { prisma } from '../../../utils/prisma';
 import { serializeDateProps } from '../../../utils/serializeDate';
 
-interface Body {
-  id: number;
-}
-
 const root: FastifyPluginAsync = async (fastify) => {
   // Finish todo API
-  fastify.post<{ Body: Body }>('/', async (request, reply) => {
-    const todo = await prisma.todo.update({
-      where: {
-        id: request.body.id,
+  fastify.post<{ Body: TodoFinishRequest; Reply: TodoPayload }>(
+    '/',
+    {
+      schema: {
+        body: TodoFinishRequestSchema,
+        response: { 200: TodoSchema },
       },
-      data: {
-        finished: true,
-      },
-      include: {
-        tags: true,
-      },
-    });
-    // TODO: エラーハンドリング
+    },
+    async (request, reply) => {
+      const todo = await prisma.todo.update({
+        where: {
+          id: request.body.id,
+        },
+        data: {
+          finished: true,
+        },
+        include: {
+          tags: true,
+        },
+      });
+      // TODO: エラーハンドリング
 
-    reply.send(serializeDateProps(todo));
-  });
+      reply.send(serializeDateProps(todo));
+    },
+  );
 };
 
 export default root;
