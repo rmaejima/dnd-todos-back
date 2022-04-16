69be2560662467c034c0115c23e0880acb295f8d
rmaejima <ryo.maejii@gmail.com>
2022-04-16 13:45:49 +0900

    Add delete tag API

---
+++ src/routes/tags/_tagId/index.ts
@@ -0,0 +1,39 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { FastifyPluginAsync } from 'fastify';
+import { prisma } from '../../../utils/prisma';
+
+type BaseParamsDictionary = {
+  tagId: number;
+};
+
+const index: FastifyPluginAsync = async (fastify) => {
+  // Delete achievement API
+  fastify.delete<{
+    Params: BaseParamsDictionary;
+  }>('/', async (request, reply) => {
+    const tagId = Number(request.params.tagId);
+    if (Number.isNaN(tagId)) {
+      throw fastify.httpErrors.notFound('タグがありません');
+    }
+
+    await prisma.tag
+      .delete({
+        where: {
+          id: tagId,
+        },
+      })
+      .catch((err) => {
+        if (
+          err instanceof PrismaClientKnownRequestError &&
+          err.code === 'P2025'
+        ) {
+          throw fastify.httpErrors.notFound('タグがありません');
+        }
+        throw err;
+      });
+
+    reply.send('タグを削除しました');
+  });
+};
+
+export default index;
